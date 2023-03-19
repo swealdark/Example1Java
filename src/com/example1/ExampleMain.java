4a27a280da9245df883f31d68149f9de8393394e
Angel <cell9125@gmail.com>
2023-03-19 17:18:11 -0500

    first commit

---
+++ src/com/example1/ExampleMain.java
@@ -0,0 +1,31 @@
+package com.example1;
+
+public class ExampleMain {
+    public static void main(String[] args){
+        //nuemricos
+        int num1 = 50;
+        long num2 = 700;
+        byte num3 = 4;
+        short num4 =10;
+            //float
+        float num5 = 40.12f;
+        double num6 = 40.78d;
+        //texto
+        String cadena1 = "hello:)";
+        char caracter1 = 'a';
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+        //imprimir
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(cadena1);
+        System.out.println(caracter1);
+        System.out.println(verdadero);
+        System.out.println(falso);
+    }
+}
